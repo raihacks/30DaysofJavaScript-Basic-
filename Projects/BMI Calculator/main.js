6da137e5be3180087eec1718f5c99ede435ee159
raihacks <siddhirai326@gmail.com>
2026-05-16 21:14:24 +0530

    ---

---
+++ Projects/BMI Calculator/main.js
@@ -0,0 +1,7 @@
+function calculate() {
+    let weight = 65;
+    let height = 18;
+    let BMI = weight / (height * height);
+    console.log(BMI);
+}
+calculate();
---
+++ Projects/Digital Clock/main.js
@@ -0,0 +1,2 @@
+let date = new Date(year,month);
+console.log(date);
