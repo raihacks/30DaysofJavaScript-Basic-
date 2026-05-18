b9b3d5d15ebea3d3998bea7eb28de5713f9b6674
raihacks <siddhirai326@gmail.com>
2026-05-18 19:40:07 +0530

    --

--- Projects/BMI Calculator/main.js
+++ Projects/BMI Calculator/main.js
@@ -5,3 +5,15 @@ function calculate() {
     console.log(BMI);
 }
 calculate();
+
+function conditionals() {
+    if (0 > age > 100) {
+        console.log("Please enter vaild age between 0 to 100.");
+    }
+    if (0 > weight > 300) {
+        console.log("Please enter vaild weight between 0 to 300 kgs.");
+    }
+    if (0 > height > 300) {
+        console.log("Please enter vaild height between 0 to 300 cm.");
+    }
+}
